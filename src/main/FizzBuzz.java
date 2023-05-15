e3bf259584dc066fa05dfcd7e963a63ab552ddab
Kevin Leonardo Ruiz Flórez <kleov@MacBook-Pro-de-Kevin.local>
2023-05-15 17:19:50 +0200

    Passing test when given 3

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -2,6 +2,9 @@ package main;
 
 public class FizzBuzz {
     public String numberToFizzBuzz(int inputNumber) {
+        if(inputNumber == 3) {
+            return "Fizz";
+        }
         return String.valueOf(inputNumber);
     }
 }
